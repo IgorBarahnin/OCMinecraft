6875071d15441ce5790a5a68d59433007cbbc86e
IgorBarahnin <ibarahnin@mail.ru>
2016-11-18 04:19:46 +0500

    Update BLUX.lua

--- BLUX.lua
+++ BLUX.lua
@@ -24,35 +24,3 @@ local function wait()
 end
 
 wait()
-
-local xNewColumnGenerationVariable = config.spaceBetweenColumns
-while true do
-	local somethingHappend = false
-	
-	local e = {event.pull(config.FPS)}
-	if birdIsAlive and (e[1] == "touch" or e[1] == "key_down") then
-		yBird = yBird - config.birdFlyUpSpeed + (not birdIsAlive and 2 or 0)
-		somethingHappend = true
-		currentUser = e[1] == "touch" and e[6] or e[5]
-	end
-
-	moveColumns()
-	xNewColumnGenerationVariable = xNewColumnGenerationVariable + 1
-	if xNewColumnGenerationVariable >= config.spaceBetweenColumns then
-		xNewColumnGenerationVariable = 0
-		generateColumn()
-	end
-
-	if not somethingHappend then
-		if yBird + bird.height - 1 < buffer.screen.height then
-			yBird = yBird + config.birdFlyDownSpeed
-		else
-			scores[currentUser] = math.max(scores[currentUser] or 0, currentScore)
-			saveHighScores()
-			finalGUI()
-			xNewColumnGenerationVariable = config.spaceBetweenColumns
-		end
-	end
-
-	drawAll()
-end
